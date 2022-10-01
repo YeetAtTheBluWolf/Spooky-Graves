8d0a2e8321ab4ca5141c41d8dbd121a0b36ac87b
Richard Bates <yeetatthebluewolf@gmail.com>
2022-10-01 19:24:58 -0400

    Foundation of the project.

---
+++ src/main/java/net/brasscord/mods/spookygraves/Spookygraves.java
@@ -0,0 +1,10 @@
+package net.brasscord.mods.spookygraves;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Spookygraves implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
---
+++ src/main/java/net/brasscord/mods/spookygraves/client/SpookygravesClient.java
@@ -0,0 +1,13 @@
+package net.brasscord.mods.spookygraves.client;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+
+@Environment(EnvType.CLIENT)
+public class SpookygravesClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
