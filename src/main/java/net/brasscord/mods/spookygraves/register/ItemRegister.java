b0770cf9fee6adc5aa887c0af5efc53c1502b5c5
RichardBatesIII <97308425+RichardBatesIII@users.noreply.github.com>
2022-10-10 08:33:42 -0400

    Update ItemRegister.java

--- src/main/java/net/brasscord/mods/spookygraves/register/ItemRegister.java
+++ src/main/java/net/brasscord/mods/spookygraves/register/ItemRegister.java
@@ -7,6 +7,12 @@ import net.minecraft.util.registry.Registry;
 
 public class ItemRegister {
 
+    /**
+    * This method registers a item easily by taking only two parameters.
+    *
+    * @param  name  This takes a string, and it's used to id the item.
+    * @param  item  This takes a item, and it's used to register the item given.
+    */
     protected static void register(String name, Item item)
     {
         Registry.register(Registry.ITEM, new Identifier(Spookygraves.id, name), item);
